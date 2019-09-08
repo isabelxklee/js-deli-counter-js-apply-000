65d05042ca76195e50b95c43953b7e5ba12f2d60
isabel klee <hello@isabelklee.co>
2019-09-08 02:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let arr = [];
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
   for (let i = 0; i < katzDeliLine.length; i++) {
-    arr.push(i + 1 + ". " + katzDeliLine[i] + " ");
+    arr.push(i + 1 + ". " + katzDeliLine[i]);
       }
         return "The line is currently: " + arr;
         } else {
