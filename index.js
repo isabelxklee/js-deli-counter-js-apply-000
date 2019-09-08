52120fe459c3b24ad1c49b745bd84fddb1f11c5f
isabel klee <hello@isabelklee.co>
2019-09-08 01:48:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(katzDeliLine) {
       for (let i = 0; i < katzDeliLine.length; i++) {
         // let number = i + 1;
       }
-        return "The line is currently: " + i+1 + ". " + katzDeliLine.join(", ") + katzDeliLine[i];
+        return "The line is currently: " + i + 1 + ". " + katzDeliLine.join(", ") + katzDeliLine[i];
     } else {
       return "The line is currently empty.";
   }
