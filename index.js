385ce1eff96c1e83c4e4c4bd3a0bf5dfb9c48a3e
isabel klee <hello@isabelklee.co>
2019-09-08 01:41:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine.push(newPerson);
+  return "Welcome, " + newPerson + ". You are number " + katzDeliLine.length + " in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let firstPerson = katzDeliLine.shift();
+    return "Currently serving " + firstPerson + ".";
+  } else {
+      return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    let i = 0;
+    return "The line is currently: " + i + ". " + katzDeliLine.join(", ");
+  } else {
+      return "The line is currently empty.";
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+      for (let i = 0; i < katzDeliLine.length; i++) {
+        let number = i + 1;
+        return "The line is currently: " + number + ". " + katzDeliLine.join(", ");
+        }
+    } else {
+      return "The line is currently empty.";
+  }
+}
